fd24d94ddf0f66dc5abc18e7a51dd8b78629f047
Conner Erich <cje00016@mix.wvu.edu>
2026-04-29 11:31:28 -0400

    updated sql so that it inclueded time data so that the time data chart could be created

--- sql/Seller_consumer_location.sql
+++ sql/Seller_consumer_location.sql
@@ -1,22 +1,23 @@
---this CTE finds what areas consume what products
-with seller_comsumer_location as (
-select
-  s.seller_city,
-  p.product_id
-from
-  sellers as s
-  full join customers as c on s.seller_city = c.customer_city
-  JOIN orders AS o ON c.customer_id = o.customer_id
-  JOIN order_items AS oi ON o.order_id = oi.order_id
-  JOIN products AS p ON oi.product_id = p.product_id
-  where s.seller_city = $1
-group by
-  s.seller_city,
-  oi.product_id,
-  p.product_id
-ORDER BY
-  s.seller_city,
-  p.product_id
+-- This CTE finds what areas consume what products over time
+WITH seller_consumer_location AS (
+    SELECT
+        s.seller_city,
+        p.product_id,
+        DATE_TRUNC('month', o.order_purchase_timestamp) AS order_month
+    FROM sellers AS s
+    FULL JOIN customers AS c  ON s.seller_city    = c.customer_city
+    JOIN orders AS o          ON c.customer_id    = o.customer_id
+    JOIN order_items AS oi    ON o.order_id       = oi.order_id
+    JOIN products AS p        ON oi.product_id    = p.product_id
+    WHERE s.seller_city = $1
+    GROUP BY
+        s.seller_city,
+        p.product_id,
+        DATE_TRUNC('month', o.order_purchase_timestamp)
+    ORDER BY
+        order_month,
+        s.seller_city,
+        p.product_id
 )
-select *
-from seller_comsumer_location
+SELECT *
+FROM seller_consumer_location;
